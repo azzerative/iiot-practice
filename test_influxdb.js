4e09561c1707574fad7f6ab049dcb009c4656960
azzerative <salehzaidan53@gmail.com>
2021-02-10 12:06:48 +0700

    InfluxDB test

---
+++ test_influxdb.js
@@ -0,0 +1,75 @@
+const Influx = require('influx');
+const express = require('express');
+const http = require('http');
+const os = require('os');
+
+const app = express();
+
+const influx = new Influx.InfluxDB({
+  host: 'localhost',
+  database: 'express_response_db',
+  schema: [
+    {
+      measurement: 'response_times',
+      fields: {
+        path: Influx.FieldType.STRING,
+        duration: Influx.FieldType.INTEGER,
+      },
+      tags: ['host'],
+    },
+  ],
+});
+
+influx
+  .getDatabaseNames()
+  .then(names => {
+    if (!names.includes('express_response_db')) {
+      return influx.createDatabase('express_response_db');
+    }
+  })
+  .then(() => {
+    http.createServer(app).listen(3000, function () {
+      console.log('Listening on port 3000');
+    });
+  })
+  .catch(err => {
+    console.error('Error creating Influx database!');
+  });
+
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    console.log(`Request to ${req.path} took ${duration}ms`);
+
+    influx
+      .writePoints([
+        {
+          measurement: 'response_times',
+          tags: { host: os.hostname() },
+          fields: { duration, path: req.path },
+        },
+      ])
+      .catch(err => {
+        console.error(`Error saving data to InfluxDB! ${err.stack}`);
+      });
+  });
+  return next();
+});
+
+app.get('/', function (req, res) {
+  setTimeout(() => res.end('Hello World!'), Math.random() * 500);
+});
+
+app.get('/times', function (req, res) {
+  influx
+    .query(`
+      SELECT * FROM response_times
+      WHERE host = ${Influx.escape.stringLit(os.hostname())}
+      ORDER BY time DESC
+      LIMIT 10
+    `)
+    .then(result => res.json(result))
+    .catch(err => res.status(500).send(err.stack));
+});
